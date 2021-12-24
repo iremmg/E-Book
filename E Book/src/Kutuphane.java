a2aea353febfa534c675321d5166d7ed703586f9
iremm_ <86535797+iremmg@users.noreply.github.com>
2021-12-24 18:35:45 +0300

    Update Kutuphane.java

--- E Book/src/Kutuphane.java
+++ E Book/src/Kutuphane.java
@@ -1,23 +1,43 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Kutuphane extends AnaSayfa{
+class kutuphane extends AnaSayfa{
 
     @Override
     public void listBook(ArrayList<String> kitapListe1) {
         Scanner keyboard = new Scanner(System.in);
         kitapListe1.addAll(kitapListe);
         System.out.println("kutuphanedeki kitaplar :\n"+kitapListe1);
-        for(int i =0 ; i<kitapListe1.size();i++){
+        ArrayList<Integer> a = new ArrayList<>();
+        for(int i =0; i<kitapListe1.size();i++){
             System.out.println(kitapListe1.get(i));
             System.out.println("Kitap sil");
             cevap= keyboard.nextInt();
             if(cevap ==1){
-                kitapListe1.remove(i);
+                a.add(i);
             }
+            else{
+                System.out.println("Oku : ");
+                cevap= keyboard.nextInt();
+                if (cevap==1){
+                    showBook(kitapListe1.get(i)); break;
+                }
+            }
+
         }
-        System.out.println(kitapListe1);
+        for(int i=0 ; i<a.size();i++){
+            int b = a.get(i);
+            kitapListe1.remove(b);
+            System.out.println(kitapListe1);
+
+        }
+
     }
+
+    private void showBook(String s) {
+        System.out.println(s);
+    }
+
 
 }
 
