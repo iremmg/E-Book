c1d58d2a64b09b116491d889a3e670606598b528
Irem_Guzel <86535797+iremmg@users.noreply.github.com>
2022-01-10 00:32:14 +0300

    Update main.java

--- E Book/src/main.java
+++ E Book/src/main.java
@@ -1,14 +1,21 @@
 public class main {
     public static void main(String[] args) {
 
-        EBook e = new EBook();
-        e.login();
-        AnaSayfa a = new AnaSayfa();
-        a.start();
-        Kutuphane k = new Kutuphane();
-        k.listBook(a.kitapListe);
-        KitapYazma k1 = new KitapYazma();
-        k1.WriteBook();
+    EBook e = new EBook();
+     e.login();
+
+     AnaSayfa a = new AnaSayfa();
+     a.start();
+
+     Kutuphane k = new Kutuphane();
+     k.listBook(a.kitapListe);
+
+     Kesfet k1 = new Kesfet();
+     k1.searchBook(a.kitapAdi );
+
+     KitapYazma ky = new KitapYazma();
+     ky.WriteBook();
+
 
 
     }}
