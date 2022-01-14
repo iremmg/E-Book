7b60c7237f3eeedf8355e69fbe6099de293a39a5
Irem_Guzel <86535797+iremmg@users.noreply.github.com>
2022-01-14 15:28:49 +0300

    Update EBook.java

--- E Book/src/EBook.java
+++ E Book/src/EBook.java
@@ -1,58 +1,97 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EBook {
-    private static kullanıcı kullanıcı;
-    AnaSayfa AnaSayfa;
-    public static void login(){
+    KullanıcıOluşturma2 KullanıcıOluşturma2;
+
+    // KayıttlıKullanıcı KayıtlıKullanıcı;
+    public void KullanıcıEkle() {
         Scanner keyboard = new Scanner(System.in);//"ISO-8859-9"
-          System.out.println("kayıt olmak ister misiniz?");
+        System.out.println("kayıt olmak ister misiniz?");
         int istek = keyboard.nextInt();
         PrintWriter girdi = null;
-        if(istek==1){
+        ArrayList<String> guvenlikSorusu = new ArrayList<>();
+        guvenlikSorusu.add("İlk evcil hayvanınızın ismi nedir");
+        guvenlikSorusu.add("En sevdiğiniz yemek");
+        guvenlikSorusu.add("En sevdiğiniz spor");
+
+        if (istek == 1) {}
             System.out.println("------------ HOŞGELDİNİZ ");
             String bosluk = keyboard.nextLine();
             System.out.print("İsim : ");
             String isim = keyboard.nextLine();
             System.out.print("Kullanıcı adı : ");
-            String kullanıcıAdı = keyboard.nextLine();
+            String KullanıcıAdı = keyboard.nextLine();
             System.out.print("E-posta : ");
             String mail = keyboard.nextLine();
             System.out.print("Şifre : ");
             String şifre = keyboard.nextLine();
-            kullanıcı = new kullanıcı(isim,kullanıcıAdı,mail,şifre); //isim,kullanıcıAdı,mail,şifre
-
-
-            File dosya = new File("C:/Users/Asus/Desktop/kullanıcı/"+kullanıcıAdı+".txt");
-            try
-
-            {
-                girdi = new PrintWriter(new FileOutputStream(dosya,true));
-            }
-            catch(FileNotFoundException ex)
-            {
-                System.out.println(kullanıcıAdı+".txt dosyası oluşturulurken hata oldu");
+            System.out.println("Güvenlik Sorusu seçiniz");
+            System.out.println("1) İlk evcil hayvanınızın ismi nedir"); //0
+            System.out.println("2) En sevdiğiniz yemek"); //1
+            System.out.println("3) En sevdiğiniz spor"); //2
+            int cevap = keyboard.nextInt();
+            String deneme1 = keyboard.nextLine();
+            System.out.println("Cevabınız : ");
+            String guvenlik = keyboard.nextLine();
+            KullanıcıOluşturma2 = new KullanıcıOluşturma2(isim, KullanıcıAdı, mail, şifre, guvenlik);
+            File dosya = new File("C:\\Users\\Asus\\Desktop\\kullanıcı\\ " + KullanıcıAdı + ".txt");
+            try {
+                girdi = new PrintWriter(new FileOutputStream(dosya, true));
+            } catch (FileNotFoundException ex) {
+                System.out.println(KullanıcıAdı + ".txt dosyası oluşturulurken hata oldu");
                 System.exit(0);
             }
-
-
-            girdi.print(isim+"\n"+kullanıcıAdı+"\n"+şifre+"\n"+mail+"\n");
-
-
-
+            girdi.print(isim + "\n" + KullanıcıAdı + "\n" + mail + "\n" + şifre + "\n" + guvenlik);
             girdi.close();
-
             System.out.println("kayıt olma işlemi başarıyla tamamlandı");
+        String guvenlik1;
+        String kullanıcıAdı;
+        String sifre;
+        int cevap1=0;
+        System.out.println("kayıtlı kullanıcı mısınız:");
+        Scanner klavye = new Scanner(System.in);
+        int kayıtlı = klavye.nextInt();
+        String düzeltme = klavye.nextLine();
+        if (kayıtlı == 1) {
+            System.out.println("kullanıcı adını giriniz: ");
+            kullanıcıAdı = klavye.nextLine();
+            System.out.println("şifrenizi giriniz: ");
+            sifre = klavye.nextLine();
+            try {
+                Scanner s = new Scanner(new File("C:\\Users\\Asus\\Desktop\\kullanıcı\\ " +KullanıcıAdı+ ".txt"));
+                while (s.hasNextLine()) {
+                    System.out.println(s.nextLine());
+                }
+                //s.close();
+            } catch (IOException e) {
+                System.out.println("hata:" + e);
+            }
+            if (kullanıcıAdı.equals(KullanıcıAdı) && şifre.equals(sifre)) {
+                System.out.println("başarılı giriş yaptınız.");
+            } else {
+                System.out.println("kullanıcı adı veya şifreyi yanlış girdiniz.");
+                System.out.println("Şifreyi Sıfırla");
+                cevap1 = klavye.nextInt();
+                if (cevap1 == 1) {
+                    System.out.println(guvenlikSorusu.get(cevap-1));
+                    String deneme = klavye.nextLine();
+                    guvenlik1 = klavye.nextLine();
+                    if (guvenlik1.equals(guvenlik)) {
+                        guvenlik = guvenlik1;
+                        System.out.print("Yeni şifrenizi giriniz : ");
 
+                        şifre = klavye.nextLine();
+                        System.out.println("Şifre sıfırlama başarılı");
 
-
-
+                    }
+                } else {
+                    System.out.println("Hatalı giriş");
+                    sifre = klavye.nextLine();
+                }
+            }
         }
 
-
     }
-
 }
